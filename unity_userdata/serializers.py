367b8845324e2a0b63441f88568a6aae5f9b6095
Han Lazarus <han.lazarus@gmail.com>
2017-10-07 22:06:29 -0700

    use a class based view

--- unity_userdata/serializers.py
+++ unity_userdata/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Game_Session
 
-
+# rename class camelcase standard
 class Game_Session_Serializer(serializers.ModelSerializer):
 	class Meta:
 		model = Game_Session
--- unity_userdata/views.py
+++ unity_userdata/views.py
@@ -1,12 +1,12 @@
-# from django.shortcuts import render, get_object_or_404
-# from django.http import Http404, HttpResponse, JsonResponse
+from django.http import Http404
 
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import Game_Session_Serializer
-from .models import Game_Session, Player
+from .models import Game_Session
 
 
 @api_view(['GET'])
@@ -21,11 +21,31 @@ def list_top_scoring_players(request, format=None):
 
 # @csrf_exempt #only needed if post is defined w/o authentication
 # urg what is post vs put again?
-@api_view(['GET', 'POST'])
-def player_game_session(request, player, format=None):
-	# if request == 'GET':
-	# may be better to query on Player
-	game_session = Game_Session.objects.get(player=player)
-	# rename class camelcase standard
-	serializer = Game_Session_Serializer(game_session)
-	return Response(serializer.data)
+class PlayerGameSession(APIView):
+	""" Retrieve, update, or delete a player's game sessions.
+	"""
+
+	def get_object(self, player):
+		# may be better to query on Player
+		try:
+			return Game_Session.objects.get(player=player)
+		except Game_Session.DoesNotExist:
+			raise Http404
+
+	def get(self, request, player, format=None):
+		game_session = self.get_object(player)
+		serializer = Game_Session_Serializer(game_session)
+		return Response(serializer.data)
+
+	def put(self, request, player, format=None):
+		game_session = self.get_object(player)
+		serializer = Game_Session_Serializer(game_session, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def delete(self, request, player, format=None):
+		game_session = self.get_object(player)
+		game_session.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
